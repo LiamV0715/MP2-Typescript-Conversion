3bb2715a01d84ad2c7336e2346d83cb3679c73f8
Liam Vermette <Liam.vermette@gmail.com>
2024-09-09 16:24:42 -0400

    stuck on errors after compiling with build

---
+++ models/comment.js
@@ -0,0 +1,8 @@
+"use strict";
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const commentSchema = new mongoose.Schema({
+    author: { type: String, default: 'Anonymous' },
+    content: { type: String, default: '' }
+});
+module.exports = mongoose.model('Comment', commentSchema);
